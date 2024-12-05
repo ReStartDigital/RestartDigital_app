c7d70b330355d56c54c4a8648f98eee1b2ef7232
McWaves3010 <samuelamoh2005@gmail.com>
2024-12-05 20:38:52 +0000

    updated

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -2,9 +2,15 @@ import React , { useEffect , useState } from "react";
 import axios from "axios";
 import { Fade } from "react-awesome-reveal";
 import Team, { Teams } from "../Reusable/Team";
+import Person1 from "../assets/images/group2.jpg";
+import Person2 from "../assets/images/group3.jpg";
+import Person4 from "../assets/images/group4.jpg";
 
 const About:React.FC = ()=>{
     const [ data , setData ] = useState<Teams[]>([]);
+    const style: React.CSSProperties = {
+        clipPath: 'polygon(0 0, 80% 0, 100% 20%, 100% 100%, 0 100%)',
+      };
     useEffect(()=>{
         async function fetchData(){
             try{
@@ -48,6 +54,42 @@ const About:React.FC = ()=>{
                         ))
                     }
                 </div>
+            </div>
+            <div className="w-full h-30vh flex justify-center items-center  p-4">
+                    <div className='w-full h-full flex justify-start items-center'>
+                        <span className='text-7xl font-Lexend capitalize'>Previous years of restart digital</span>
+                    </div>
+            </div>
+            <div className='w-full h-full flex justify-center items-center p-4 gap-4- bg-gray-100'>
+                <div className="w-[80%] h-full  rounded-lg flex justify-center items-center flex-col p-4">
+                    <div className='w-full flex justify-start items-center p-4'>
+                        <span className='font-Poppins font-bold text-2xl'>Restart Digital events in the years</span>
+                    </div>
+                    <div className='w-full flex justify-start items-center p-4'>
+                        <span className='font-Poppins text-sm'>
+                            The TECH Event, hosted by Restart Digital last year, was a groundbreaking gathering of technology enthusiasts, industry leaders, and innovators. 
+                            Focused on fostering collaboration and knowledge sharing, the event featured interactive workshops, panel discussions, and keynote speeches by renowned tech pioneers. 
+                            Attendees explored cutting-edge trends in AI, blockchain, and software development, gaining insights into future technological advancements. 
+                            Networking sessions provided a platform to connect with like-minded professionals and forge valuable partnerships.
+                            Restart Digital’s commitment to innovation shone through its seamless organization and vibrant atmosphere. 
+                            The TECH Event not only inspired participants but also strengthened the company’s reputation as a digital leader.
+                        </span>
+                    </div>
+                </div>
+                <div className="w-full h-full  rounded-lg flex justify-center items-center gap-4 p-3">
+                    <div className="w-full h-full rounded-lg" style={style}>
+                        <img src={Person1} alt="" className='w-full h-full object-cover relative rounded-lg'/>
+                    </div>
+                    <div className='w-full h-full flex justify-center items-center flex-col gap-4'>
+                        <div className="w-full h-full rounded-lg" style={style}>
+                            <img src={Person2} alt="" className='w-full h-full object-cover relative rounded-lg'/>
+                        </div>
+                        <div className="w-full h-full rounded-lg" style={style}>
+                            <img src={Person4} alt="" className='w-full h-full object-cover relative rounded-lg'/>
+                        </div>
+                    </div>
+                    
+                </div>  
             </div>
         </section>
     )
